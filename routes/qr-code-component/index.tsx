6f0e1e86c624cfbd2e67fc1d944775c3d2b6a9b3
sppam lite <sppamlitte@gmail.com>
2023-02-26 10:39:34 -0500

    basic layout finished

--- routes/qr-code-component/index.tsx
+++ routes/qr-code-component/index.tsx
@@ -14,14 +14,25 @@ const Home = ({ route }: PageProps) => {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap"
           rel="stylesheet"
         />
         <link rel="stylesheet" href="/styles/base.css" />
         <link rel="stylesheet" href={`/styles${route}/styles.css`} />
       </Head>
       <main>
-        qr code component
+        <h1 class="sr-only">QR Code Component</h1>
+        <section>
+          <div class="qr-code">
+          </div>
+          <div class="description">
+            <h2>Improve your front-end skills by building projects</h2>
+            <p>
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </div>
+        </section>
       </main>
     </>
   );
