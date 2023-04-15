9de502d5604a44490d88ea983dcfb53bbae02b79
sppam lite <sppamlitte@gmail.com>
2023-04-15 08:16:20 -0400

    add duration calculation

--- islands/age-calculator.tsx
+++ islands/age-calculator.tsx
@@ -1,5 +1,6 @@
 import { JSX } from "preact";
 import { useSignal } from "@preact/signals";
+import { intervalToDuration, parse } from "date-fns";
 import { NumberInput } from "../components/age-calculator-app/NumberInput.tsx";
 import { AgeDisplay } from "../components/age-calculator-app/AgeDisplay.tsx";
 import { CalculateButton } from "../components/age-calculator-app/CalculateButton.tsx";
@@ -8,6 +9,11 @@ type Props = {
   defaultDay?: number;
   defaultMonth?: number;
   defaultYear?: number;
+};
+type Duration = {
+  years?: number;
+  months?: number;
+  days?: number;
 };
 
 type InputHandler = JSX.GenericEventHandler<HTMLInputElement>;
@@ -20,6 +26,24 @@ const AgeCalculator = ({
   const dayOfBirth = useSignal<number>(defaultDay || 0);
   const monthOfBirth = useSignal<number>(defaultMonth || 0);
   const yearOfBirth = useSignal<number>(defaultYear || 0);
+  const duration = useSignal<Duration | null>(null);
+
+  const handleCalculate = () => {
+    const start = new Date(
+      yearOfBirth.value,
+      monthOfBirth.value,
+      dayOfBirth.value,
+    );
+    const { years, months, days } = intervalToDuration({
+      start,
+      end: new Date(),
+    });
+    duration.value = {
+      years,
+      months,
+      days,
+    };
+  };
 
   const handleValueChange: InputHandler = ({ currentTarget }) => {
     const id = currentTarget.id;
@@ -59,8 +83,12 @@ const AgeCalculator = ({
             label="YEAR"
           />
         </form>
-        <CalculateButton />
-        <AgeDisplay />
+        <CalculateButton onClick={handleCalculate} />
+        <AgeDisplay
+          years={duration.value?.years}
+          months={duration.value?.months}
+          days={duration.value?.days}
+        />
       </div>
     </section>
   );
